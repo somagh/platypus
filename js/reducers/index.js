2f99b296bbbef8c725ab73a8c0c9ffa03659310c
amirrezam <amirrezamozayani@gmail.com>
2017-09-15 15:24:03 +0430

    server .

--- js/reducers/index.js
+++ js/reducers/index.js
@@ -2,6 +2,6 @@ import { combineReducers } from 'redux'
 import user from './user'
 import transaction from './transaction'
 
-const rootReducer = combineReducers({ user, transaction })
+const rootReducer = combineReducers({ user })
 
 export default rootReducer
